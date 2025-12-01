6c186ce07077c002cce0b65aa7364df33f081221
thedarkwad <159988414+thedarkwad@users.noreply.github.com>
2025-12-01 12:20:04 -0500

    Update api.update._index.tsx

--- app/routes/api.update._index.tsx
+++ app/routes/api.update._index.tsx
@@ -10,6 +10,39 @@ export async function action({
   request,
 }: ActionFunctionArgs) {
 
+  let cleanUpImages = async (chain: Chain, chainId: string) => {
+    if (!/^[a-zA-Z0-9_-]*$/.test(chainId))
+      return;
+    try {
+      let altFormsWithImages = Object.keys(chain.altforms).filter((id) => !!chain.altforms[Number(id)].imageUploaded);
+      let imageDirectory = path.join(appRoot, "public/user_images", chainId);
+      if (!existsSync(imageDirectory)) return;
+      const files = await fs.readdir(imageDirectory);
+      const fileToDelete = files.filter((file) => !altFormsWithImages.includes(path.parse(file).name));
+      for (let file of fileToDelete) {
+        await fs.unlink(path.join(imageDirectory, file));
+      }
+    } catch (e) { console.error(e); }
 
-  return json({ success: false });
+  }
+
+  const body = await request.json();
+  let doc = await loadChain(body.id);
+  if (!doc)
+    throw new Response(null, {
+      status: 500,
+      statusText: "Save Error",
+    });
+
+  if (body.edits != doc.edits) {
+    return json({ editRace: true, error: "An edit race has been detected. This is often caused by having the same chain open in two tabs. Please ensure that you would like to overwrite any other changes before saving your chain again. Autosave has been disabled." });
+  }
+  try {
+    await updateChain(body.updates, doc);
+  } catch(e) {
+    console.error(e);
+    return json({ error: "Unknown error encounted while saving. Please try again." })
+  }
+  cleanUpImages(doc.chain!, body.id);
+  return json({ success: true });
 }
